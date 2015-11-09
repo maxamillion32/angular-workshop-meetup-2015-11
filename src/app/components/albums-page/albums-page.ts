4e83bc476f4656cbcca1cdb7469118fa77aa9802
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2015-11-09 19:30:08 -0300

    [Step 3] Add albums page

---
+++ src/app/components/albums-page/albums-page.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'albums-page',
+  templateUrl: 'app/components/albums-page/albums-page.html',
+  styleUrls: ['app/components/albums-page/albums-page.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class AlbumsPage {
+
+  constructor() {}
+
+}
--- src/app/music.ts
+++ src/app/music.ts
@@ -1,12 +1,12 @@
 import {Component, bootstrap} from 'angular2/angular2';
-
+import {AlbumsPage} from './components/albums-page/albums-page';
 
 @Component({
   selector: 'music-app',
   providers: [],
   templateUrl: 'app/music.html',
   styleUrls: ['app/music.css'],
-  directives: [],
+  directives: [AlbumsPage],
   pipes: []
 })
 export class MusicApp {
